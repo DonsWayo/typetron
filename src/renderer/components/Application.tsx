a45ff00150b776bae74009fe3776adb9396785ee
DonsWayo <djwayomix@gmail.com>
2020-04-05 22:59:22 +0200

    remove redux, more easy to select any store

--- src/renderer/components/Application.tsx
+++ src/renderer/components/Application.tsx
@@ -1,12 +1,9 @@
 import { hot } from 'react-hot-loader/root';
 import * as React from 'react';
 
-import Counter from './Counter';
-
 const Application = () => (
     <div>
         Hello World from Electron!
-        <Counter />
     </div>
 );
 
